513ce3f1d78a7790e65e3134efd423ef9aa43a1d
Christian Kaisermann <christian@kaisermann.me>
2016-10-14 14:16:23 -0300

    Changed ajax add_action calls to action__methodName

--- src/ajax.php
+++ src/ajax.php
@@ -5,8 +5,8 @@ namespace App;
 $methods = [];
 
 foreach ($methods as $method) {
-    add_action('wp_ajax_nopriv_'.$method, __NAMESPACE__.'\\'.$method);
-    add_action('wp_ajax_'.$method, __NAMESPACE__.'\\'.$method);
+    add_action('wp_ajax_nopriv_'.$method, __NAMESPACE__.'\\action__'.$method);
+    add_action('wp_ajax_'.$method, __NAMESPACE__.'\\action__'.$method);
 }
 
 // Ajax methods
