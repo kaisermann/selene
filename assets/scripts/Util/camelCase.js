40137c5a7a2411a1a7d85db0ff736a6f7b48bf78
Christian Kaisermann <christian@kaisermann.me>
2016-10-21 20:01:44 -0200

    Removed strict from camelCase

--- assets/scripts/Util/camelCase.js
+++ assets/scripts/Util/camelCase.js
@@ -1,5 +1,3 @@
-'use strict';
-
 export default str => `${str.charAt(0).toLowerCase()}${str.replace(/[\W_]/g, '|').split('|')
   .map(part => `${part.charAt(0).toUpperCase()}${part.slice(1)}`)
   .join('')
