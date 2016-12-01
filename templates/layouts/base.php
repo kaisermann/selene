7f020c544a245f8bcc2b901147437b0b9ca90758
Christian Kaisermann <christian@kaisermann.me>
2016-12-01 18:29:46 -0200

    Modified base.php

--- templates/layouts/base.php
+++ templates/layouts/base.php
@@ -2,26 +2,28 @@
 <html <?php language_attributes(); ?>>
 <?php get_template_part( 'partials/head' ); ?>
 <body <?php body_class(); ?>>
-	<?php
-	do_action( 'get_header' );
-	get_template_part( 'partials/header' );
-	?>
-	<div class="page__container" role="document">
-		<div class="page__content">
-			<main class="main" role="main">
-				<?php include App\template()->main(); ?>
-			</main>
-			<?php if ( App\display_sidebar() ) : ?>
-				<aside class="sidebar" role="complementary">
-					<?php App\template_part( 'partials/sidebar' ); ?>
-				</aside>
-			<?php endif; ?>
+	<div class="page-wrapper" role="document">
+		<div class="page-container">
+			<?php
+			do_action( 'get_header' );
+			get_template_part( 'partials/header' );
+			?>
+			<div class="page-content">
+				<main class="main" role="main">
+					<?php include App\template()->main(); ?>
+				</main>
+				<?php if ( App\display_sidebar() ) : ?>
+					<aside class="sidebar" role="complementary">
+						<?php App\template_part( 'partials/sidebar' ); ?>
+					</aside>
+				<?php endif; ?>
+			</div>
+			<?php
+			do_action( 'get_footer' );
+			get_template_part( 'partials/footer' );
+			wp_footer();
+			?>
 		</div>
 	</div>
-	<?php
-	do_action( 'get_footer' );
-	get_template_part( 'partials/footer' );
-	wp_footer();
-	?>
 </body>
 </html>
