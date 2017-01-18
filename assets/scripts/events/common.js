9b7447d9d9d8fe99149afbb986ff21b6e0823398
Christian Kaisermann <christian@kaisermann.me>
2017-01-18 17:53:16 -0200

    Eslint extending js standard

--- assets/scripts/events/common.js
+++ assets/scripts/events/common.js
@@ -1,8 +1,8 @@
 export default {
-  init() {
+  init () {
     // JavaScript to be fired on all pages
   },
-  finalize() {
+  finalize () {
     // JavaScript to be fired on all pages, after page specific JS is fired
   },
-};
+}
