7cca9c819e7cd286f4a0f532e2e98b84e3e5002b
Christian Kaisermann <christian@kaisermann.me>
2017-02-17 11:41:38 -0200

    ⛓  Share controllers between templates

--- src/controllers.php
+++ src/controllers.php
@@ -2,11 +2,18 @@
 
 namespace App;
 
-$controllers = [ 'global' ];
+$controllers = [
+	'global',
+];
 
-function registerController( $scope, $fn ) {
-	add_filter( "sage/template/$scope/data", $fn, 10, 2 );
+function registerController( $scopes, $fn ) {
+	if ( ! is_array( $scopes ) ) {
+		$scopes = [ $scopes ];
+	}
+	foreach ( $scopes as $scope ) {
+		add_filter( "sage/template/{$scope}/data", $fn, 10, 2 );
+	}
 }
 
 // Includes the files listed on $controllers
-includeArrayOfFiles($controllers, 'src/controllers');
+includeArrayOfFiles( $controllers, 'src/controllers' );
--- src/controllers/global.php
+++ src/controllers/global.php
@@ -6,12 +6,12 @@ registerController('global', function( $data, $template ) {
 	return $data + [
 		'current_template' => basename( $template ,'.blade.php' ),
 		'primary_nav_args' => [
-			'theme_location' => 'primary_navigation',
-			'menu_class' => 'nav',
-			'walker' => new \BEM\MenuWalker,
-			'block' => 'header__menu',
-			'menu_class' => 'nav header__menu',
-			'container' => false,
+		'theme_location' => 'primary_navigation',
+		'menu_class' => 'nav',
+		'walker' => new \BEM\MenuWalker,
+		'block' => 'header__menu',
+		'menu_class' => 'nav header__menu',
+		'container' => false,
 		],
 	];
 });
