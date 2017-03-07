f524168478e55eab0cc049d8db3e845d0beec44e
Christian Kaisermann <christian@kaisermann.me>
2017-03-07 10:57:39 -0300

    Automatic controller loading

--- src/controllers.php
+++ src/controllers.php
@@ -2,16 +2,16 @@
 
 namespace App;
 
-$controllers = [
-	'global',
-];
+$controllers = array_map(function($val) {
+	return basename($val, '.php');
+}, glob( __DIR__ . '/controllers/*.php'));
 
 function registerController( $scopes, $fn ) {
 	if ( ! is_array( $scopes ) ) {
 		$scopes = [ $scopes ];
 	}
 	foreach ( $scopes as $scope ) {
-		add_filter( "sage/template/{$scope}/data", function( $data, $template ) use ($fn) {
+		add_filter( "sage/template/{$scope}/data", function( $data, $template ) use ( $fn ) {
 			return $data + $fn($data, $template);
 		}, 10, 2 );
 	}
