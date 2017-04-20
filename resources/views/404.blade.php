1b516421e4bc5ecef6cbce4f9909805da1b7ab1d
Christian Kaisermann <christian@kaisermann.me>
2017-04-20 16:44:27 -0300

    Removed have_posts from 404

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,10 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-  @if (!have_posts())
-    <div class="alert -warning">
-      {{ __('404 - Page Not Found', 'selene') }}
-    </div>
-    {!! get_search_form(false) !!}
-  @endif
+  <div class="alert -warning">
+    {{ __('404 - Page Not Found', 'selene') }}
+  </div>
+  {!! get_search_form(false) !!}
 @stop
