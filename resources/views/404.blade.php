cf61897a07ffa7a785a5b7535bff183cc0f32e20
Vitor Paladini <vitor@paladini.in>
2017-04-20 16:32:58 -0300

    small fixes

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
+  @if (!have_posts())
     <div class="alert -warning">
       {{ __('404 - Page Not Found', 'selene') }}
     </div>
     {!! get_search_form(false) !!}
+  @endif
 @stop
