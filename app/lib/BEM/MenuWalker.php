2b310b44f89dc6867c7803b007026463b95beb9c
Christian Kaisermann <christian@kaisermann.me>
2017-07-13 16:12:47 -0300

    Support custom classes on BEM MenuWalker

--- app/lib/BEM/MenuWalker.php
+++ app/lib/BEM/MenuWalker.php
@@ -5,7 +5,7 @@ namespace BEM;
 // Better BEM classes for wp_nav_menu https://codepad.co/snippet/xzEkTh25
 class MenuWalker extends \Walker_Nav_Menu {
 
-	public function start_lvl( &$output, $depth = 0, $args = array() ) {
+	public function start_lvl( &$output, $depth = 0, $args = [] ) {
 		$block = isset( $args->block ) ? $args->block : explode( ' ', $args->menu_class );
 		$block = is_array( $block ) ? $block[0] : $block;
 		if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
@@ -19,7 +19,7 @@ class MenuWalker extends \Walker_Nav_Menu {
 		$output .= "{$n}{$indent}<ul class=\"{$block}__sub-menu\">{$n}";
 	}
 
-	public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+	public function start_el( &$output, $item, $depth = 0, $args = [], $id = 0 ) {
 		if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
 			$t = '';
 			$n = '';
@@ -29,19 +29,27 @@ class MenuWalker extends \Walker_Nav_Menu {
 		}
 		$indent = ( $depth ) ? str_repeat( $t, $depth ) : '';
 		$classes = $this->prepare_el_classes( $item, $args, $depth );
-		
+
 		$args = apply_filters( 'nav_menu_item_args', $args, $item, $depth );
 
-		$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth ) );
+		$class_names = join( ' ', apply_filters(
+				'nav_menu_css_class',
+				array_filter( $classes ),
+				$item,
+				$args,
+				$depth
+			)
+		);
+
 		$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
 
 		//$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args, $depth );
 		//$id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
 		$id = '';
-		
+
 		$output .= $indent . '<li' . $id . $class_names .'>';
 
-		$atts = array();
+		$atts = [];
 		$atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
 		$atts['target'] = ! empty( $item->target )     ? $item->target     : '';
 		$atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
@@ -65,14 +73,14 @@ class MenuWalker extends \Walker_Nav_Menu {
 		$item_output .= $args->link_before . $title . $args->link_after;
 		$item_output .= '</a>';
 		$item_output .= $args->after;
-		
+
 		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
 	}
 
-	public function prepare_el_classes( &$item, $args = array(), $depth = 0 ) {
+	public function prepare_el_classes( &$item, $args = [], $depth = 0 ) {
 		$block = isset( $args->block ) ? $args->block : explode( ' ', $args->menu_class );
 		$block = is_array( $block ) ? $block[0] : $block;
-		$classes = array( $block . '__item' );
+		$classes = [ $block . '__item' ];
 
 		if ( $item->current ) {
 			$classes[] = '-current';
@@ -88,6 +96,12 @@ class MenuWalker extends \Walker_Nav_Menu {
 
 		if ( in_array( 'menu-item-has-children', (array) $item->classes ) ) {
 			$classes[] = '-has-children';
+		}
+
+		foreach($item->classes as $class) {
+			if ( substr($class, 0, 5) !== 'menu-' ) {
+				$classes[] = $class;
+			}
 		}
 
 		if ( $depth ) {
