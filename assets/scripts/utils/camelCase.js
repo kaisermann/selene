2805d4445e1b42112cba2923abfe7c6c02cf148f
Christian Kaisermann <christian@kaisermann.me>
2017-07-18 10:48:55 -0300

    Update dependencies

--- assets/scripts/utils/camelCase.js
+++ assets/scripts/utils/camelCase.js
@@ -1,8 +1,6 @@
 // the most terrible camelizer on the internet, guaranteed!
 export default str =>
-  `${str
-    .charAt(0)
-    .toLowerCase()}${str
+  `${str.charAt(0).toLowerCase()}${str
     .replace(/[\W_]/g, '|')
     .split('|')
     .map(part => `${part.charAt(0).toUpperCase()}${part.slice(1)}`)
