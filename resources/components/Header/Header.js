d42e3ee57be95a2ae16f6e7555ce9e28dc9b8eb5
Christian Kaisermann <christian@kaisermann.me>
2017-07-24 16:16:34 -0300

    Add esc to close menu

--- resources/components/Header/Header.js
+++ resources/components/Header/Header.js
@@ -1,16 +1,16 @@
+const toggleOverlay = e => {
+  if (e && typeof e.keyCode !== 'undefined' && e.keyCode !== 27) return
+  if (document.body.classList.toggle('is-header-overlay-active')) {
+    window.addEventListener('keyup', toggleOverlay)
+  } else {
+    window.removeEventListener('keyup', toggleOverlay)
+  }
+}
+
 export default {
   init () {
     document
       .querySelector('.js-burger')
-      .addEventListener('click', function toggleOverlay (e) {
-        if (e && e.keyCode !== undefined && e.keyCode !== 27) return
-        if (document.body.classList.toggle('is-header-overlay-active')) {
-          // Open
-          window.addEventListener('keyup', toggleOverlay)
-        } else {
-          // Close
-          window.removeEventListener('keyup', toggleOverlay)
-        }
-      })
+      .addEventListener('click', toggleOverlay)
   },
 }
