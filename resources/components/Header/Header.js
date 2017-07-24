b2d8362e37b7149128de831c4f41684b2dc28c16
Christian Kaisermann <christian@kaisermann.me>
2017-07-24 16:04:03 -0300

    Make header listen to body class instead

--- resources/components/Header/Header.js
+++ resources/components/Header/Header.js
@@ -1,24 +1,16 @@
-let burger
-let headerOverlay
-
-const toggleOverlay = e => {
-  if (e && e.keyCode !== undefined && e.keyCode !== 27) return
-  if (
-    burger.classList.toggle('is-active') &
-    headerOverlay.classList.toggle('is-visible')
-  ) {
-    // Open
-    window.addEventListener('keyup', toggleOverlay)
-  } else {
-    // Close
-    window.removeEventListener('keyup', toggleOverlay)
-  }
-}
-
 export default {
   init () {
-    burger = document.querySelector('.js-burger')
-    headerOverlay = document.querySelector('.js-header-overlay')
-    burger.addEventListener('click', toggleOverlay)
+    document
+      .querySelector('.js-burger')
+      .addEventListener('click', function toggleOverlay (e) {
+        if (e && e.keyCode !== undefined && e.keyCode !== 27) return
+        if (document.body.classList.toggle('is-header-overlay-active')) {
+          // Open
+          window.addEventListener('keyup', toggleOverlay)
+        } else {
+          // Close
+          window.removeEventListener('keyup', toggleOverlay)
+        }
+      })
   },
 }
