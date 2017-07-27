165bd19c669e057d27d5ccaef1a9887bab15c4bd
Christian Kaisermann <christian@kaisermann.me>
2017-07-27 13:33:09 -0300

    Add commented out admin_menu items hide action

--- app/admin.php
+++ app/admin.php
@@ -6,11 +6,16 @@ namespace App;
 // Enqueues admin.css on login page and dashboard
 add_action( 'admin_enqueue_scripts', 'App\action__admin_enqueue_scripts', 100 );
 add_action( 'login_enqueue_scripts', 'App\action__admin_enqueue_scripts', 100 );
+
 // Removes default dashboard metaboxes
 add_action( 'admin_init', 'App\action__admin_init' );
 add_action( 'admin_init', 'App\action__default_sizes_crop' );
+
 // Removes WP logo and comments menu from admin bar
 add_action( 'admin_bar_menu','App\action__trim_adminbar', 100 );
+
+// Removes some unused dashboard menu items
+// add_action( 'admin_menu','App\action__trim_adminmenu' );
 
 remove_action( 'welcome_panel', 'wp_welcome_panel' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
@@ -19,12 +24,16 @@ remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 // Filters
 // Sets login page logo redirecting to home url
 add_filter( 'login_headerurl', 'App\filter__login_headerurl' );
+
 // Moves yoast SEO (if available) metabox to a lower position
 add_filter( 'wpseo_metabox_prio', 'App\filter__wpseo_metabox_prio' );
+
 // Removes the "help" section on top of admin pages
 add_filter( 'contextual_help', 'App\filter__contextual_help', 11, 3 );
+
 // Edits the admin left footer text
 // add_filter( 'admin_footer_text',  'App\filter__admin_footer_text', 11);
+
 // Edits the admin right footer text
 // add_filter( 'update_footer',  'App\filter__update_footer', 11, 1 );
 
@@ -74,6 +83,12 @@ function action__trim_adminbar( $wp_admin_bar ) {
 	$wp_admin_bar->remove_node( 'view-site' );
 	$wp_admin_bar->remove_menu( 'customize' );
 	$wp_admin_bar->remove_menu( 'comments' );
+}
+
+function action__trim_adminmenu() {
+	// remove_menu_page( 'edit-comments.php' );
+	// remove_submenu_page( 'options-general.php', 'options-writing.php' );
+	// remove_submenu_page( 'options-general.php', 'options-discussion.php' );
 }
 
 // Filters
