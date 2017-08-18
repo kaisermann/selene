451bec1f8a9fe60a473cbc1fe5590c27f04a98e8
Christian Kaisermann <christian@kaisermann.me>
2017-08-18 10:58:47 -0300

    Fix for plugins that don't enqueue their scripts properly

--- app/helpers.php
+++ app/helpers.php
@@ -52,6 +52,9 @@ function config($key = null, $default = null)
  */
 function template($file, $data = [])
 {
+    if (remove_action('wp_head', 'wp_enqueue_scripts', 1)) {
+        wp_enqueue_scripts();
+    }
     return sage('blade')->render($file, $data);
 }
 
