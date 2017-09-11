d3f8e7391eaed2d332ddf7975297317d8d396cd0
Christian Kaisermann <christian@kaisermann.me>
2017-09-11 11:03:07 -0300

    Small refactor of sample field

--- app/fields/sample.php
+++ app/fields/sample.php
@@ -7,17 +7,25 @@ use StoutLogic\AcfBuilder\FieldsBuilder;
 /*
  * ACF Builder documentation: https://github.com/StoutLogic/acf-builder/wiki
  * ACF PHP documentation: https://www.advancedcustomfields.com/resources/register-fields-via-php/
+ * Guide to registering ACF fields via PHP: https://bwap.ch/guide-to-registering-acf-fields-via-php/
  * Use the 'ACF_FIELDS_DIR' constant to reference the app/fields directory (for partials inclusions)
  */
 /*
+
+/** Field Group setup */
 $sample_field = new FieldsBuilder('sample_field', [
     'hide_on_screen' => ['the_content'],
 ]);
+
+/** Fields setup */
 $sample_field
     ->addText('title')
-    ->addWysiwyg('content')
-    ->setLocation('post_type', '==', 'page')
+    ->addWysiwyg('content');
+
+
+/** Field Group location setup */
+$sample_field
+    ->setLocation('page_template', '==', 'views/template-custom.blade.php')
         ->or('post_type', '==', 'post');
 
-return $sample_field;
-*/
+// return $sample_field;
