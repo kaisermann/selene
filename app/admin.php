9f860c306e58b67d7fc6b4b6bd9ebd34418bcdbc
Christian Kaisermann <christian@kaisermann.me>
2017-09-12 10:49:21 -0300

    Rename wpdl.styl to WPDLS

--- app/admin.php
+++ app/admin.php
@@ -6,7 +6,12 @@ namespace App;
  * Enqueue 'admin.css' and 'login.css' on login page and dashboard
  */
 $adminEnqueueFn = function () {
-    wp_enqueue_style('selene/wpdl.css', asset_path('styles/wpdl.css'), false, null);
+    wp_enqueue_style(
+        'selene/dashboard-login.css',
+        asset_path('styles/dashboard-login.css'),
+        false,
+        null
+    );
 };
 add_action('admin_enqueue_scripts', $adminEnqueueFn, 100);
 add_action('login_enqueue_scripts', $adminEnqueueFn, 100);
