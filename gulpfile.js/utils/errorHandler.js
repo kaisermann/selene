7791faae3c58f358aae3adbd6cc8a017adeae1d4
Christian Kaisermann <christian@kaisermann.me>
2017-10-31 21:58:07 -0200

    Print error stack

--- gulpfile.js/utils/errorHandler.js
+++ gulpfile.js/utils/errorHandler.js
@@ -3,6 +3,7 @@ const util = require('gulp-util')
 /** Simple main error handler */
 module.exports = function (err) {
   util.beep()
-  util.log(err.message)
+  util.log(util.colors.red('Error message: ' + err.message))
+  util.log(`Stack:\n ${err.stack}`)
   this.emit('end')
 }
