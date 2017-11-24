3d3094f1b96397c693347a26dbf6999fe2ee5f6b
Christian Kaisermann <christian@kaisermann.me>
2017-11-24 17:53:39 -0200

    Update Header.js

--- resources/components/Header/Header.js
+++ resources/components/Header/Header.js
@@ -1,16 +1,19 @@
-const toggleOverlay = e => {
-  if (e && typeof e.keyCode !== 'undefined' && e.keyCode !== 27) return
-  if (document.body.classList.toggle('is-header-overlay-active')) {
-    window.addEventListener('keyup', toggleOverlay)
-  } else {
-    window.removeEventListener('keyup', toggleOverlay)
-  }
-}
-
 export default {
   init () {
-    document
-      .querySelector('.js-burger')
-      .addEventListener('click', toggleOverlay)
+    const burgerEl = document.querySelector('.js-burger')
+    burgerEl.addEventListener('click', function toggleOverlay (e) {
+      if (e && typeof e.keyCode !== 'undefined' && e.keyCode !== 27) return
+
+      if (document.body.classList.toggle('is-header-overlay-active')) {
+        window.addEventListener('keyup', toggleOverlay)
+      } else {
+        window.removeEventListener('keyup', toggleOverlay)
+      }
+
+      burgerEl.setAttribute(
+        'aria-pressed',
+        !(this.getAttribute('aria-pressed') === 'true')
+      )
+    })
   },
 }
