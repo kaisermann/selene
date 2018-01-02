74add5e2220b77e3b42568c7f7abfa594f49aacd
Christian Kaisermann <christian@kaisermann.me>
2018-01-02 15:52:54 -0200

    Update filters.php

--- app/filters.php
+++ app/filters.php
@@ -138,9 +138,10 @@ add_filter('body_class', function (array $classes) {
 
     /** Regex patterns to replace class names */
     $replacePatterns = [
-        '/page-template-template-(.*)(?:-blade)?/' => 'template-$1', // Simplifies template classes
+        '/page-template-template-(.*)/' => 'template-$1', // Simplifies template classes
         '/page-template-(.*)/' => 'template-$1',
         '/post-type-archive-(.*)/' => 'archive-$1', // Simplifies custom-post-type-archive
+        '/(.*)-blade$/' => '$1', // Removes -blade from classes
     ];
 
     /** Add post/page slug if not present */
