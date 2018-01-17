1828ef1e8b024b183089f1fd1a2d7d4d43718c55
Christian Kaisermann <christian@kaisermann.me>
2018-01-17 11:42:22 -0200

    Move rollup setup one directory up

--- gulpfile.js/rollup/plugins.js
+++ gulpfile.js/rollup/plugins.js
@@ -3,9 +3,9 @@ const rollUpCommonjs = require('rollup-plugin-commonjs')
 const rollUpNodeResolve = require('rollup-plugin-node-resolve')
 const rollUpNodebuiltins = require('rollup-plugin-node-builtins')
 const rollUpSizes = require('rollup-plugin-sizes')
-const rollUpSizeReporter = require('./size-reporter')
 
-const Flags = require('../../Flags')
+const Flags = require('../Flags')
+const rollUpSizeReporter = require('./size-reporter')
 
 /** List of Rollup plugins to be used */
 const plugins = [
