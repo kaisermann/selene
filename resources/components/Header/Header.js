447b2466497b19d85e76ecafadf26bd8b91bf7c5
Christian Kaisermann <christian@kaisermann.me>
2018-02-07 14:32:39 -0200

    Refactor Header.js

--- resources/components/Header/Header.js
+++ resources/components/Header/Header.js
@@ -1,19 +1,20 @@
+let isOpen = false
+
 export default {
   init () {
     const burgerEl = document.querySelector('.js-burger')
-    burgerEl.addEventListener('click', function toggleOverlay (e) {
-      if (e && typeof e.keyCode !== 'undefined' && e.keyCode !== 27) return
-
-      if (document.body.classList.toggle('is-header-overlay-active')) {
-        window.addEventListener('keyup', toggleOverlay)
-      } else {
-        window.removeEventListener('keyup', toggleOverlay)
+    if (burgerEl) {
+      const toggleMenu = () => {
+        isOpen = document.body.classList.toggle('is-header-overlay-active')
+        burgerEl.setAttribute('aria-expanded', isOpen)
       }
 
-      burgerEl.setAttribute(
-        'aria-expanded',
-        !(burgerEl.getAttribute('aria-expanded') === 'true')
-      )
-    })
+      burgerEl.addEventListener('click', toggleMenu)
+      window.addEventListener('keyup', e => {
+        if (isOpen && e.keyCode === 27) {
+          toggleMenu()
+        }
+      })
+    }
   },
 }
